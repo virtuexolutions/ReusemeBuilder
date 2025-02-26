5d95c258b799ce791f209d4a239890afc76b54d5
Baber11 <syedbaber115@gmail.com>
2025-02-26 19:02:35 +0500

    changes

---
+++ SRC/appNavigation.js
@@ -0,0 +1,179 @@
+import React, {useEffect, useState} from 'react';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import navigationService from './navigationService';
+import {useSelector} from 'react-redux';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Drawer from './Drawer/Drawer';
+import LoginScreen from './Screens/LoginScreen';
+import VerifyNumber from './Screens/VerifyNumber';
+import ChangePassword from './Screens/ChangePassword';
+import ResetPassword from './Screens/ResetPassword';
+import VerifyEmail from './Screens/VerifyEmail';
+import WalkThroughScreen from './Screens/WalkthroughScreen';
+import {moderateScale} from 'react-native-size-matters';
+import {enableScreens} from 'react-native-screens';
+
+enableScreens();
+const AppNavigator = () => {
+  const token = useSelector(state => state.authReducer.token);
+
+  const RootNav = createNativeStackNavigator();
+  const RootNavLogged = createNativeStackNavigator();
+
+  const AppNavigatorContainer = () => {
+    // const firstScreen =
+    //   walkThrough == false
+    //     ? 'WalkThroughScreen'
+    //     : token == null
+    //     ?'LoginScreen'
+    //     // ? 'Start'
+    //     : 'MyDrawer';
+
+    return (
+      <NavigationContainer ref={navigationService.navigationRef}>
+        <RootNav.Navigator
+          initialRouteName={'LoginScreen'}
+          screenOptions={{headerShown: false}}>
+          <RootNav.Screen name="MyDrawer" component={MyDrawer} />
+          <RootNav.Screen
+            name="WalkThroughScreen"
+            component={WalkThroughScreen}
+          />
+          <RootNav.Screen name="LoginScreen" component={LoginScreen} />
+          <RootNav.Screen name="VerifyEmail" component={VerifyEmail} />
+          <RootNav.Screen name="ResetPassword" component={ResetPassword} />
+          <RootNav.Screen name="ChangePassword" component={ChangePassword} />
+          <RootNav.Screen name="VerifyNumber" component={VerifyNumber} />
+        </RootNav.Navigator>
+      </NavigationContainer>
+    );
+  };
+
+  return <AppNavigatorContainer />;
+};
+
+// export const TabNavigation = () => {
+//   const Tabs = createBottomTabNavigator();
+//   return (
+//     <Tabs.Navigator
+//       // tabBar={(props) => {
+//       //   return (
+//       //     <LinearGradient
+//       //       colors={['red', 'blue']}
+
+//       //       start={[1, 0]}
+//       //       end={[0, 0]}
+//       //     >
+//       //       <BottomTabBar
+//       //         {...props}
+//       //         style={{ backgroundColor: 'transparent' }}
+//       //       />
+//       //     </LinearGradient>
+//       //   );
+//       // }}
+//       screenOptions={({route}) => ({
+//         headerShown: false,
+//         tabBarShowLabel: false,
+//         tabBarStyle: {
+//           // backgroundColor:'pink',
+//           // backgroundColor: Color.red,
+//           // borderTopLeftRadius:15,
+//           // borderTopRightRadius:15,
+//           // paddingVertical:5
+//         },
+//         tabBarIcon: ({focused}) => {
+//           let iconName;
+//           let color = Color.theme2;
+//           let size = moderateScale(20, 0.3);
+//           let type = Ionicons;
+
+//           // if (route.name === 'HomeScreen') {
+//           //   iconName = focused ? 'home' : 'home-outline';
+
+//           //   color = focused ? Color.theme2 : Color.white;
+//           //   size = focused ? moderateScale(30, 0.3) : moderateScale(20, 0.3);
+//           // } else
+//           if (route.name === 'Donation') {
+//             iconName = focused ? 'donate' : 'donate';
+//             type = FontAwesome5;
+//             color = focused ? Color.theme2 : Color.white;
+//             size = focused ? moderateScale(30, 0.3) : moderateScale(20, 0.3);
+//           } else if (route.name === 'StoreScreen') {
+//             iconName = focused ? 'cart' : 'cart';
+//             color = focused ? Color.theme2 : Color.white;
+//             size = focused ? moderateScale(30, 0.3) : moderateScale(20, 0.3);
+//           } else if (route?.name == 'Campaigns') {
+//             size = focused ? moderateScale(30, 0.3) : moderateScale(20, 0.3);
+//           } else {
+//             iconName = focused ? 'settings-sharp' : 'settings-outline';
+//             color = focused ? Color.theme2 : Color.white;
+//             size = focused ? moderateScale(30, 0.3) : moderateScale(20, 0.3);
+//           }
+//           return route.name == 'Campaigns' ? (
+//             <View
+//               style={{
+//                 borderWidth: 5,
+//                 borderColor: Color.lightGrey,
+//                 height: moderateScale(60, 0.3),
+//                 width: moderateScale(60, 0.3),
+//                 borderRadius: moderateScale(30, 0.3),
+//                 backgroundColor: Color.theme2,
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 marginTop: moderateScale(-30, 0.3),
+//               }}>
+//               <Icon
+//                 name={'search'}
+//                 as={Feather}
+//                 color={Color.white}
+//                 size={size}
+//               />
+//             </View>
+//           ) : (
+//             <Icon name={iconName} as={type} color={color} size={size} />
+//           );
+//         },
+//         tabBarShowLabel: false,
+//         tabBarBackground: () => (
+//           <View style={{flex: 1}}>
+//             <LinearGradient
+//               start={{x: 0, y: 0}}
+//               end={{x: 0, y: 1}}
+//               colors={Color.tabBarGradient}
+//               style={{height: windowHeight * 0.1}}
+//             />
+//           </View>
+//         ),
+//       })}>
+//       {/* <Tabs.Screen name={'HomeScreen'} component={HomeScreen} /> */}
+//       {/* <Tabs.Screen name={'Donation'} component={Donation} />
+//       <Tabs.Screen name={'Campaigns'} component={Campaigns} />
+//       {/* <Tabs.Screen name={'BibleCategories'} component={BibleCategories} /> */}
+//       {/* <Tabs.Screen name={'StoreScreen'} component={StoreScreen} /> */}
+//       <Tabs.Screen name={'Settings'} component={Settings} />
+//     </Tabs.Navigator>
+//   );
+// };
+
+export const MyDrawer = () => {
+  const DrawerNavigation = createDrawerNavigator();
+  const firstScreen = 'HomeScreen';
+  return (
+    <DrawerNavigation.Navigator
+      drawerContent={props => <Drawer {...props} />}
+      initialRouteName={'Home'}
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          width: '80%',
+          borderTopRightRadius: moderateScale(120, 0.6),
+          borderBottomRightRadius: moderateScale(120, 0.6),
+        },
+      }}>
+      <DrawerNavigation.Screen name="Home" component={Home} />
+    </DrawerNavigation.Navigator>
+  );
+};
+
+export default AppNavigator;
